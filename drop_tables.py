d9a95943d0991561a8359e0ae7c0959dff52784b
Rick <eralon18@gmail.com>
2023-05-30 13:44:13 +0200

    .

--- drop_tables.py
+++ drop_tables.py
@@ -1,6 +1,6 @@
 def drop_all_tables(cursor):
     table_names = ["Users", "User_looking_to_rent", "user_rents_out_property", "Property", "Property_match", "Property_details", "Message"]
-    types = ["prop_type", "match_status_type", "gender_type", "energy_label_type", "furnished_type", "gender_roommates_type"]
+    types = ["prop_type", "match_status_type", "gender_type", "energy_label_type", "furnished_type", "gender_roommates_type", "statust_type"]
     try:
         for table in table_names:
             cursor.execute(f"DROP TABLE IF EXISTS {table} CASCADE;")
